aa8100b5adc51f99a985d67d25586c110b36e306
simon-lzw <secretsimonlo@outlook.com>
2016-12-28 09:32:38 +0800

    2

--- src/lzwGIT/test.java
+++ src/lzwGIT/test.java
@@ -4,5 +4,11 @@ public class test {
 	int a=0, b=0 ;
 	int i=a+b;
 	private static String name;
+	public static String getName() {
+		return name;
+	}
+	public static void setName(String name) {
+		test.name = name;
+	}
 	
 }
